5abacef01314a584064b4d56518b7734ab9a121c
Agney <agney.menon@bigbinary.com>
2020-03-14 23:00:08 +0530

    move text avatar to own file

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,26 +1,11 @@
-import React, {
-  FC,
-  createContext,
-  useRef,
-  useEffect,
-  useState,
-  useContext,
-} from 'react';
+import React, { FC, createContext, useContext } from 'react';
 import styled from 'styled-components';
 
 import AvatarWrapper from './AvatarWrapper';
+import TextAvatar from './TextAvatar';
 import useHasImageLoaded from './useHasImageLoaded';
 
 export type IShape = 'circle' | 'square';
-
-const Text = styled('p')<{ scale: number }>`
-  margin: 0;
-  vertical-align: middle;
-  font-weight: bold;
-  white-space: nowrap;
-  text-transform: uppercase;
-  transform: scale(${props => props.scale});
-`;
 
 const Image = styled.img`
   object-fit: cover;
@@ -33,81 +18,6 @@ const defaultBackgrounds = [
   '#0be881',
   '#f53b57',
 ];
-
-/**
- * Get sum representing all characters in text.
- * @param text
- */
-const sumOfCharacters = (text: string) => {
-  let sum = 0;
-  for (let i = 0; i < text.length; i += 1) {
-    sum += text.charCodeAt(i);
-  }
-  return sum;
-};
-
-const TextAvatar: FC<{
-  htmlWidth: string;
-  htmlHeight: string;
-  className: string;
-  shape: IShape;
-  text: string;
-  backgrounds: string[];
-  bgColor?: string;
-  textColor?: string;
-}> = ({
-  htmlWidth,
-  htmlHeight,
-  className,
-  shape,
-  text,
-  bgColor,
-  textColor,
-  backgrounds,
-}) => {
-  const backgroundColor = (() => {
-    if (bgColor) {
-      return bgColor;
-    }
-    const index = sumOfCharacters(text) % backgrounds.length;
-    return backgrounds[index];
-  })();
-
-  const containerRef = useRef<HTMLDivElement>(null);
-  const textRef = useRef<HTMLParagraphElement>(null);
-  const [scale, setScale] = useState<number>(1);
-
-  useEffect(() => {
-    const container = containerRef.current;
-    const text = textRef.current;
-    if (!container || !text) {
-      return;
-    }
-    const containerWidth = container.offsetWidth;
-    const textWidth = text.offsetWidth;
-    if (containerWidth - 8 < textWidth) {
-      setScale((containerWidth - 8) / textWidth);
-    } else {
-      setScale(1);
-    }
-  }, [text, htmlWidth, htmlHeight]);
-
-  return (
-    <AvatarWrapper
-      htmlWidth={htmlWidth}
-      htmlHeight={htmlHeight}
-      className={className}
-      shape={shape}
-      bgColor={backgroundColor}
-      textColor={textColor}
-      ref={containerRef}
-    >
-      <Text ref={textRef} scale={scale}>
-        {text}
-      </Text>
-    </AvatarWrapper>
-  );
-};
 
 const ImageAvatar: FC<{
   htmlWidth: string;
