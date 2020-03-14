c758d9a38aa70a1c6651f46fce669f0107c2ea45
Agney <agney.menon@bigbinary.com>
2020-03-15 01:04:27 +0530

    update to include tests

--- test/Avatar.test.tsx
+++ test/Avatar.test.tsx
@@ -1,11 +1,33 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import useHasImageLoaded from '../src/useHasImageLoaded';
+
 import { Avatar } from '../src';
+
+jest.mock('../src/useHasImageLoaded');
 
 describe('Avatar', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Avatar />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<Avatar />);
+  });
+
+  it('should show text', () => {
+    const text = 'AB';
+    const { getByText } = render(<Avatar text={text} />);
+    expect(getByText(text)).toBeDefined();
+  });
+
+  it('should show image with alt', () => {
+    const img = {
+      src: 'http://some-image.jpg',
+      alt: 'some-image',
+    };
+    (useHasImageLoaded as jest.Mock).mockReturnValue(true);
+    const { getByAltText } = render(
+      <Avatar src={img.src} imageAlt={img.alt} />
+    );
+    expect(getByAltText(img.alt)).toBeDefined();
   });
 });
