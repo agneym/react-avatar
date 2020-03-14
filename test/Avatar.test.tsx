01af2482865cdd13259b31aea3fd4fe1c87dfbc6
Agney <agney.menon@bigbinary.com>
2020-03-14 23:28:31 +0530

    Add dom testing library

--- test/Avatar.test.tsx
+++ test/Avatar.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Avatar } from '../src';
 
-describe('it', () => {
+describe('Avatar', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Avatar />, div);
