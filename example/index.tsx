cd8a16d148f83352bc583f06f271d46c64da80b5
Agney <agney.menon@bigbinary.com>
2020-03-15 00:13:38 +0530

    Add font to demo

--- example/index.tsx
+++ example/index.tsx
@@ -6,6 +6,10 @@ import { Avatar, AvatarContext } from '../.';
 const App = () => {
   const contextValue = React.useMemo(() => ({
     backgrounds: ['#000000', '#DD2C00', '#6200EA', '#3F51B5'],
+    textProcessor: (text: string) => {
+      const textSnippets = text.split(' ');
+      return textSnippets.map((snippet) => snippet.charAt(0)).join('');
+    }
   }), []);
   return (
     <div>
@@ -28,17 +32,26 @@ const App = () => {
         htmlWidth='150px'
         text="Fallback"
       />
+      <Avatar
+        src='https://gravar.com/avatar/eabdeb636ae18d3278648438aecd54f0?s=400&d=robohash&r=x'
+        htmlWidth='150px'
+        text="Fallback"
+      />
       <AvatarContext.Provider value={contextValue}>
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', fontSize: '1.2em' }}>
           <Avatar
             htmlWidth='150px'
-            text="Fallback"
+            text="Happy"
             backgrounds={['red']}
             textColor='white'
           />
           <Avatar
             htmlWidth='150px'
-            text="AJ"
+            text="Natsu Dragneel"
+          />
+          <Avatar
+            htmlWidth='150px'
+            text="Erza Scarlett"
           />
         </div>
       </AvatarContext.Provider>
