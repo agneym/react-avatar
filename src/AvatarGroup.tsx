f9f8715e97b11afcd0db49c2d6dae3d9328b2631
Agney <agney.menon@bigbinary.com>
2020-03-21 23:31:23 +0530

    move in the mapping

--- src/AvatarGroup.tsx
+++ src/AvatarGroup.tsx
@@ -1,8 +1,20 @@
-import React, { FC, Children, isValidElement, ReactChildren } from "react";
+import React, { FC, Children, isValidElement, cloneElement } from 'react';
+import styled from 'styled-components';
 
-function cleanChildren(children: ReactChildren) {
+function cleanChildren(children: Element[]) {
   return Children.toArray(children).filter(child => isValidElement(child));
 }
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+const AvatarWrapper = styled.div<{ marginLeft: number, zIndex: number }>`
+  margin-left: ${props => props.marginLeft}px;
+  z-index: ${props => props.zIndex};
+`;
 
 interface IProps {
   /** Maximum number of avatars to be shown */
@@ -10,13 +22,30 @@ interface IProps {
   /** Spacing between each avatar */
   spacing?: number;
   /** children Avatar components */
-  children: ReactChildren;
+  children: Element[];
 }
 
-const AvatarGroup: FC<IProps> = ({ max = 3, spacing = -3, children }) => {
+const AvatarGroup: FC<IProps> = ({ max = 3, spacing = -30, children }) => {
   const validChildren = cleanChildren(children);
-  const totalCount = validChildren.length;
-  return null;
-}
+
+  return (
+    <Flex>
+      {validChildren.map((child, index) => {
+    if(index > max) {
+      return null;
+    }
+    if(index === max) {
+      return <p>+ More</p>
+    }
+    const first = index === 0;
+    return (
+      <AvatarWrapper marginLeft={first ? 0 : spacing} zIndex={index}>
+        {child}
+      </AvatarWrapper>
+    );
+  })}
+    </Flex>
+  );
+};
 
 export default AvatarGroup;
